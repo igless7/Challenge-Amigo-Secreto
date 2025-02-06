cbaf259431bab4d928447d8ee21c34aa047fb001
igless7 <98765638+igless7@users.noreply.github.com>
2025-02-05 20:31:36 -0600

    funcion actualizar lista amigo

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-let amigoInput = document.querySelector(".input-name").value
+let lista = document.getElementById("listaAmigos");
 function agregarAmigo(){
-    if(amigoInput == ""){
-      //validar que el campo no esté vacío
-      alert("Por favor, inserte un nombre.");
-    } else{
-        //agregar amigo a la lista
-        amigos.push(amigoInput);
-    }
-    //limpiar input
-    amigoInput = "";
+    let amigoInput = document.querySelector(".input-name").value;
+  if (amigoInput == "") {
+    //validar que el campo no esté vacío
+    alert("Por favor, inserte un nombre.");
+  } else {
+    //agregar amigo a la lista
+    amigos.push(amigoInput);
+    actualizarListaAmigos();
+  }
+  // Limpiar el input
+  document.querySelector(".input-name").value = "";
+  console.log(amigos);
+}
+
+function actualizarListaAmigos(){
+    lista.innerHTML = "";
+    amigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
 }
