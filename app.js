d7dcf5157f2ad819855f67facd012dbeb40f357c
igless7 <98765638+igless7@users.noreply.github.com>
2025-02-05 20:16:32 -0600

    funcion agregarAmigo a la lista amigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigoInput = document.querySelector(".input-name").value
+function agregarAmigo(){
+    if(amigoInput == ""){
+      //validar que el campo no esté vacío
+      alert("Por favor, inserte un nombre.");
+    } else{
+        //agregar amigo a la lista
+        amigos.push(amigoInput);
+    }
+    //limpiar input
+    amigoInput = "";
+}
